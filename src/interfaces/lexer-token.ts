430ca7c306cbb2886f68ea26b7ecba02ef452332
Stefan Behrendt <behrendt@onexip.com>
2019-01-03 09:21:03 +0100

    strange conflicts???

--- src/interfaces/lexer-token.ts
+++
@@ -1,7 +0,0 @@
-import {LexerTokenCategory} from '../enums/lexer/lexerTokenCategory';
-
-export interface LexerToken {
-    which: LexerTokenCategory;
-    value: string;
-    offset: { x: number, y: number };
-}
--- src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of blitzbasicscript
- */
-
-export * from './lib/blitz-basic-script.service';
-export * from './lib/blitz-basic-script.component';
-export * from './lib/blitz-basic-script.module';
